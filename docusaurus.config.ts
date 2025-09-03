2a12c46d3ddc8f9073316ce37959576ae8918301
Ethan Arrowood <ethan@arrowood.dev>
2025-09-03 10:21:46 -0600

    demo tabulated code blocks and add support for bash language

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -276,6 +276,7 @@ const config: Config = {
 		prism: {
 			theme: prismThemes.github,
 			darkTheme: prismThemes.dracula,
+			additionalLanguages: ['bash']
 		},
 	} satisfies Preset.ThemeConfig,
 	scripts,
