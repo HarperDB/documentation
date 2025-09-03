e71e78ac5f2730293fa31444b82eaa17953b9c65
Nathan Heskew <nathan@harperdb.io>
2025-09-03 10:03:33 -0700

    iterate on index cards and add the 4th getting started card

---
+++ src/components/CustomDocCardList.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import { filterDocCardListItems } from '@docusaurus/plugin-content-docs/client';
+import Heading from '@theme/Heading';
+import styles from './CustomDocCardList.module.css';
+
+interface CustomDocCardListProps {
+	items: any[];
+	columns?: number;
+	className?: string;
+}
+
+function CustomDocCard({ item }: { item: any }) {
+	return (
+		<Link href={item.href} className={clsx('card padding--lg', styles.customCard)}>
+			<Heading as="h2" className={styles.cardTitle}>
+				{item.label}
+			</Heading>
+			{item.description && <p className={styles.cardDescription}>{item.description}</p>}
+		</Link>
+	);
+}
+
+export default function CustomDocCardList({ items, columns = 2, className }: CustomDocCardListProps) {
+	const filteredItems = filterDocCardListItems(items);
+
+	return (
+		<div className={clsx(styles.cardGrid, className)} style={{ '--columns': columns } as React.CSSProperties}>
+			{filteredItems.map((item, index) => (
+				<div key={index}>
+					<CustomDocCard item={item} />
+				</div>
+			))}
+		</div>
+	);
+}
