8082a0805a91b353349f15d33158e53f651aefe0
Ethan Arrowood <ethan@arrowood.dev>
2025-11-25 09:24:57 -0700

    Hide version dropdown on non-versioned paths

---
+++ src/theme/NavbarItem/DocsVersionDropdownNavbarItem.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import DocsVersionDropdownNavbarItem from '@theme-original/NavbarItem/DocsVersionDropdownNavbarItem';
+import {useLocation} from '@docusaurus/router';
+import type {Props} from '@theme/NavbarItem/DocsVersionDropdownNavbarItem';
+
+function checkPathname(pathname: string) {
+  return pathname.startsWith('/fabric') || pathname.startsWith('/release-notes');
+}
+
+export default function DocsVersionDropdownNavbarItemWrapper(props: Props) {
+  const location = useLocation();
+
+  return checkPathname(location.pathname) ? null : <DocsVersionDropdownNavbarItem {...props} />;
+}
